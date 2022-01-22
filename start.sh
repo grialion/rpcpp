295d90d023982eb63c65717bded3a44f6375e619
grialion <48643945+grialion@users.noreply.github.com>
2022-01-22 12:15:12 +0100

    fix start script

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/lib"
 export LD_LIBRARY_PATH
 
-./rpcfetch.sh 0 &
+./rpcfetch.sh &
 cd build
+sleep 3 # rpcpp tries to read the fifo too fast, need to wait before it's created
 ./rpcpp
 pkill -x rpcfetch.sh
